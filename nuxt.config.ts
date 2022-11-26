e10a78aafd143040fdb6e1cb9f1ae0b827ee4acc
Jack Domleo <jackdomleo7@gmail.com>
2022-11-26 22:32:55 +0000

    Install modern-normalize

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,13 @@ export default defineNuxtConfig({
   typescript: {
     typeCheck: true,
     strict: true
+  },
+  css: [
+    'modern-normalize/modern-normalize.css'
+  ],
+  vite: {
+    css: {
+      devSourcemap: true
+    }
   }
 })
